b4e51913397d81440c7c14809e6408901c7127c1
noviceplayer <noviceplayer1001@gmail.com>
2026-01-21 22:36:46 +0800

    Update game.js

--- static/game.js
+++ static/game.js
@@ -54,17 +54,40 @@ setInterval(function() {
 }, 1000 / 60);
 
 var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = 700;
+canvas.height = 400;
 var context = canvas.getContext('2d');
+
+// The URL of the image you want to load
+const imageUrl = 'image/bg.png'; // Example image URL
+const imagePUrl = 'image/p.png'; // Example image URL
+// Create a new Image object
+const img = new Image();
+const player_img = new Image();
+// Set the onload handler. The image must be loaded before it can be drawn.
+img.onload = function() {
+    // Draw the image onto the canvas at coordinates (0, 0)
+    // ctx.drawImage(img, 0, 0);
+	context.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
+player_img.onload = function() {
+    // Draw the image onto the canvas at coordinates (0, 0)
+    // ctx.drawImage(img, 0, 0);
+	//context.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
+// Set the source of the image. This starts the loading process.
+img.src = imageUrl;
+player_img.src = imagePUrl;
+
 socket.on('state', function(players) {
   console.log(players);
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'green';
+  context.clearRect(0, 0, 700, 400);
+  // context.fillStyle = 'green';
   for (var id in players) {
     var player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
+    context.drawImage(player_img, player.x, player.y);
+    //context.beginPath();
+    //context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    //context.fill();
   }
 });
