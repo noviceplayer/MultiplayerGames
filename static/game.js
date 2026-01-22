758686d963c8b2cb9028b926c125f9ba16edee53
noviceplayer <noviceplayer1001@gmail.com>
2026-01-22 11:43:04 +0800

    Update game.js

--- static/game.js
+++ static/game.js
@@ -1,15 +1,5 @@
 var socket = io();
 
-let lastTouchEnd = 0;
-document.addEventListener('touchend', (event) => {
-  const now = (new Date()).getTime();
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault();
-  }
-  lastTouchEnd = now;
-}, false);
-
-
 var movement = {
   up: false,
   down: false,
