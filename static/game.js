0aef34b2a4b36161bcab8f0fb447e312830927b4
noviceplayer <noviceplayer1001@gmail.com>
2026-02-03 14:07:44 +0800

    Update game.js

--- static/game.js
+++ static/game.js
@@ -95,7 +95,7 @@ function drawStroked(text, x, y) {
 
 // The URL of the image you want to load
 const imageUrl = 'https://raw.githubusercontent.com/noviceplayer/MultiplayerGames/refs/heads/master/images/bg.png'; // Example image URL
-const imagePUrl = 'https://raw.githubusercontent.com/noviceplayer/MultiplayerGames/refs/heads/master/images/p.png'; // Example image URL
+const imagePUrl = 'https://raw.githubusercontent.com/noviceplayer/MultiplayerGames/refs/heads/master/images/s0.png'; // Example image URL
 // Create a new Image object
 const img = new Image();
 const player_img = new Image();
